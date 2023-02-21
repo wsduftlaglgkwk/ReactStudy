5faaef49ec5790f41d058b1471f346459a67a6f3
wsduftlaglgkwk <123063520+wsduftlaglgkwk@users.noreply.github.com>
2023-02-21 10:21:11 +0900

    nextjs 스터디 시작~~

---
+++ 박규동/src/pages/index.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+export default function Home() {
+  return (
+    <>
+      <h1>
+        <Link href="/">WEB</Link>
+      </h1>
+      <ol>
+        <li>
+          <Link href="/read/1">html</Link>
+        </li>
+        <li>
+          <Link href="/read/2">css</Link>
+        </li>
+      </ol>
+      <article>
+        <h2>Welcome</h2>
+        Hello, Web!!
+      </article>
+      <ul>
+        <li>
+          <Link href="/create">Create</Link>
+        </li>
+        <li>
+          <Link href="/update">Update</Link>
+        </li>
+        <li>
+          <Link href="/delete">Delete</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
